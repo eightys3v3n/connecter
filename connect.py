58c2e72cbe1dd6af65b9a0592efb4dd4501d2342
Terrence <eightys3v3n@gmail.com>
2018-11-09 12:08:29 -0700

    Removed outdated files

--- connect.py
+++ connect.py
@@ -1,37 +1,38 @@
-#!/bin/python3
+#!/usr/bin/python3
 
-# tries a variety of means to connect to a specified host. (direct, through tor)
-
+"""
+tries a variety of means to connect to a specified host. (direct, through tor)
+"""
 
 from os import system
 from sys import argv
 from getopt import getopt
 from subprocess import Popen, PIPE
 from time import sleep
+from itertools import chain
 
-
-true  = True
-false = False
 
 global verbose
-verbose = false
 global wait_time
-wait_time = '4'
 global tor_address
-tor_address = '127.0.0.1'
 global tor_port
-tor_port = '9060'
 global hosts
+
+verbose = False
+wait_time = '4'
+tor_address = '127.0.0.1'
+tor_port = '9060'
 hosts   = {
   'server':{
-    'dns'  :'dns name pointing to server',
-    'onion':'onion url pointing to my server',
-    'port' :'443'
+    'user' :'username',
+    'dns'  :'DNS name or IP for server',
+    'onion':'Onion address for same server',
+    'port' :'22'
   }
 }
 
 
-def ncat_ping( address, port, wait=None, proxy=None ):
+def ncat_ping(address, port, wait=None, proxy=None):
   """
   Tests to see if a connection can be established to the given address on the given port.
 
@@ -41,8 +42,8 @@ def ncat_ping( address, port, wait=None, proxy=None ):
   proxy   | address:port of socks4 proxy to connect through
 
   Returns:
-    true if successfull connection
-    false otherwise
+    True if successfull connection
+    False otherwise
   """
   if wait is None:
     wait = wait_time
@@ -57,12 +58,12 @@ def ncat_ping( address, port, wait=None, proxy=None ):
   if verbose:
     print('ncat returned', r)
 
-  # false means no connection
-  # true means successfull connection
+  # False means no connection
+  # True means successfull connection
   return not p.returncode
 
 
-def tor_resolve( host ):
+def tor_resolve(host):
   """
   Uses tor-resolve to resolve an onion address to an ip address for programs that can't change their dns resolution server to tor
 
@@ -97,10 +98,10 @@ def try_dns(host):
   Try to connect to the hosts dns name or ip address
 
   Returns:
-    true if successfully connected
-    false otherwise
+    True if successfully connected
+    False otherwise
   """
-  return ncat_ping( hosts[host]['dns'], hosts[host]['port'] )
+  return ncat_ping(hosts[host]['dns'], hosts[host]['port'])
 
 
 def try_onion(host):
@@ -108,14 +109,18 @@ def try_onion(host):
   Try to connect to the hosts onion address
 
   Returns:
-    true if successfully connected
-    false otherwise
+    True if successfully connected
+    False otherwise
   """
   address = tor_resolve( host )
-  return ncat_ping( address, hosts[host]['port'], proxy=tor_address+':'+tor_port )
+  return ncat_ping(address, hosts[host]['port'], proxy=tor_address+':'+tor_port)
 
 
-def connect( target, mode ):
+def ParseOptions( options ):
+  return ' '.join( options )
+
+
+def connect( target, mode, options=[], verbose=False ):
   """
   Tries to open an ssh connection to the given target
 
@@ -129,12 +134,19 @@ def connect( target, mode ):
   """
   global hosts
 
+  options.append('-p{}'.format(hosts[target]['port']))
+
   if mode == 'auto' or mode == 'dns':
     if try_dns(target):
       if verbose:
         print("using dns")
 
-      system( 'ssh -p'+hosts[target]['port']+' '+hosts[target]['dns'] )
+      options = ' '.join(options)
+      command = 'ssh '+options+' '+hosts[target]['user']+'@'+hosts[target]['dns']
+      if verbose:
+        open('/tmp/ssh_command', 'w').write(command)
+        print("ssh command written to /tmp/ssh_command")
+      system(command)
       return
 
   if mode == 'auto' or mode == 'onion':
@@ -142,27 +154,58 @@ def connect( target, mode ):
       if verbose:
         print("using onion")
 
-      system( 'ssh -oStrictHostKeyChecking=no -C -oProxyCommand=\'ncat -w '+wait_time+' --proxy-type socks4 --proxy '+tor_address+':'+tor_port+' %h %p\' -p'+hosts[target]['port']+' anonymous@'+hosts[target]['redirect_ip'] )
+      options.extend( [
+        '-oStrictHostKeyChecking=no',
+        '-C',
+        '-oProxyCommand=\'ncat -w '+wait_time+' --proxy-type socks4 --proxy '+tor_address+':'+tor_port+' %h $p\'',
+      ] )
+
+      options = ' '.join( options )
+      system( 'ssh '+options+' '+hosts[target]['user']+'@'+hosts[target]['dns'])
       return
 
   else:
     raise Exception( "invalid mode", mode )
 
 
+def PrintHelp():
+  helps = {
+    ('--help',): "Prints this help text",
+    ('--verbose',): "Print more information",
+    ('--target', 'name'): "Connect to this host configuration",
+    ('--dns',): "Connect using the DNS name only",
+    ('--onion',): "Connect using the onion DNS name only",
+    ('--local', 'local_port:remote_ip:remote_port'): "Port forward local_port through the SSH connection and from the remote PC to remote_ip:remote_port",
+    ('--remote', 'remote_port:local_ip:local_port'): "Port forward remote_port on the remote PC through the SSH connection to this PC then to local_ip:local_port",
+  }
+  optionlen = max([len(h[0]) for h in helps.keys()])
+  for help, desc in helps.items():
+    if len(help) > 1:
+      format = '{:{width}} '
+      format += ' '.join(['{}' for h in help[1:]])
+      format += ' | {desc}'
+      print(format.format(*help, desc=desc, width=optionlen))
+    else:
+      format = '{:{width}} {desc}'
+      print(format.format(*help, desc=desc, width=optionlen))
+
+
 def main():
   global verbose, hosts
   target = None # alias for the computer to connect to
   mode = 'auto'
+  extra_options = []
 
-  opts, args = getopt( argv[1:], 'hvt:do', ['help', 'verbose', 'target=', 'dns', 'onion'] )
+  opts, args = getopt( argv[1:], 'hvt:doL:R:', [ 'help', 'verbose', 'target=', 'dns', 'onion', 'local=', 'remote=' ] )
 
   # parse options and their arguments
   for opt,arg in opts:
     if opt in ['-h', '--help']:
-      print('help text')
+      PrintHelp()
 
     elif opt in ['-v', '--verbose']:
-      verbose = true
+      verbose = True
+      extra_options.append('-v')
 
     elif opt in ['-t', '--target']:
       target = arg
@@ -173,6 +216,12 @@ def main():
     elif opt in ['-o', '--onion']:
       mode = 'onion'
 
+    elif opt in ['-L', '--local']:
+      extra_options.append( '-L'+arg )
+
+    elif opt in ['-R', '--remote']:
+      extra_options.append( '-R'+arg )
+
   # parse arguments
   for arg in args:
     if target is None:
@@ -182,7 +231,15 @@ def main():
   if target is None:
     target = 'server'
 
-  connect( target, mode )
+  build_in_rules = []
+
+  # Can append ports to this list to have them forwarded from the local host to the remote host for every connection.
+  # build_in_rules.append(8080)
+
+  for port in build_in_rules:
+    extra_options.append('-L{l}:{p}:{r}:{p}'.format(l='127.0.0.2', r='127.0.0.1', p=port))
+
+  connect(target, mode, options=extra_options, verbose=verbose)
 
 
 if __name__ == '__main__':
